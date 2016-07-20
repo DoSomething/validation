e9bd7efb14d88d33ddfcdd2eff5a7e5f9a9714c3
David Furnes <dfurnes@dosomething.org>
2016-07-20 10:24:54 -0400

    Use DS webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,19 @@
 'use strict';
 
-var pkg = require('./package.json');
-var fender = require('fender');
+var config = require('@dosomething/webpack-config');
 
-/**
- * Use Fender for nice and easy builds.
- * @see http://npmjs.com/fender
- */
-var config = fender(pkg, {
-  bundles: {
-    'validation': './index.js'
+module.exports = config({
+  entry: {
+    validation: './index.js'
+  },
+
+  // Don't bundle the 'jquery' package with the library (validation.js), but
+  // instead load from `jQuery` global variable or AMD/CJS package.
+  externals: {
+    'jquery': {
+      root: 'jQuery',
+      commonjs2: 'jquery',
+      amd: 'jquery'
+    }
   },
 });
-
-// Well... with one hack. We want to export this as a UMD module
-// so it can be used in CommonJS builds or as a global in the browser.
-config.output.library = 'DSValidation';
-config.output.publicPath = '/dist/';
-config.output.libraryTarget = 'umd';
-
-config.hot = false;
-config.watch = false;
-config.keepalive = true;
-config.devtool = '#inline-source-map';
-
-module.exports = config;
